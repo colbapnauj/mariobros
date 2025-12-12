fe4ec4d7255ffa3d2fc321b934316f0b0ad00744
Jorge <jorge.ayuque08@gmail.com>
2025-12-11 21:19:23 -0500

    Codigo de personaje Shinobi

--- ESCENAS/Personaje/pj_shinobi.gd
+++ ESCENAS/Personaje/pj_shinobi.gd
@@ -1,46 +1,52 @@
 extends CharacterBody2D
 
-@onready var anin = $AnimatedSprite2D
-const SPEED = 200
-const GRAVITY = 10
+@export var velocidad: float = 150.0
+@export var fuerza_salto: float = 350.0
+var gravedad: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var anim = $AnimatedSprite2D
+
+@onready var respawn_point = get_parent().get_node("RespawnPoint").global_position
+
+func jump():
+	velocity.y = fuerza_salto
 
 
 func _physics_process(delta):
-	
 	# Aplicar gravedad
-	if !is_on_floor():
-		velocity.y += GRAVITY
-	
-	# Obtener dirección de movimiento horizontal
-	var input_dir = Input.get_axis("ui_left","ui_right")
-	velocity.x = input_dir*SPEED
-	
+	if not is_on_floor():
+		velocity.y += gravedad * delta
+
+	# Movimiento horizontal
+	var input_dir := Input.get_axis("ui_left", "ui_right")
+	velocity.x = input_dir * velocidad
+
+	# Saltar
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y -= 200
-	
-	
-	# Manejar animaciones
-	_handle_animations(input_dir)
-	move_and_slide()
-	
-func _handle_animations(input_dir):
-	# Si está en el aire
-	if !is_on_floor():
-		if velocity.y < 0:
-			anin.play("Jump") # Animación de salto
-		else:
-			anin.play("Hurt") # Animación de caída
-		return
+		velocity.y = -fuerza_salto
 		
-	# Si está en el suelo
+		
+	var screen_rect = get_viewport().get_visible_rect()
+
+	if global_position.y > screen_rect.size.y + 50:
+		global_position = respawn_point
+		velocity = Vector2.ZERO
+
+
+	# Animación + flip
+	update_animation(input_dir)
+
+	# Aplicar física
+	move_and_slide()
+
+
+func update_animation(input_dir: float):
 	if input_dir != 0:
-		# Personaje se mueve
-		anin.play("Run")
-		# Voltear sprite según dirección
-		if input_dir > 0:
-			anin.flip_h = false # Mirando a la derecha
-		elif input_dir < 0:
-			anin.flip_h = true # Mirando a la izquierda
+		anim.flip_h = input_dir < 0
+
+	if not is_on_floor():
+		anim.play("Jump")
+	elif input_dir == 0:
+		anim.play("Idle")
 	else:
-		# Personaje quieto
-		anin.play("Idle")
+		anim.play("Run")
